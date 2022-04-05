0999d42b489904e3e0ff3a887f1dfd23cffb2bfe
Gameknight77YT <24jschroeder@gmail.com>
2022-04-05 12:34:48 -0500

    added colorsensor test

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,10 +4,14 @@
 
 package frc.robot;
 
+import com.revrobotics.ColorSensorV3;
+
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.cscore.UsbCamera;
+import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -21,6 +25,7 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer robotContainer;
+  ColorSensorV3 colorSensor = new ColorSensorV3(I2C.Port.kOnboard);
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -47,6 +52,14 @@ public class Robot extends TimedRobot {
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
+    Color color = colorSensor.getColor();
+    double IR = colorSensor.getIR();
+    double dis = colorSensor.getProximity();
+    SmartDashboard.putNumber("blue", color.blue);
+    SmartDashboard.putNumber("red", color.red);
+    SmartDashboard.putNumber("green", color.green);
+    SmartDashboard.putNumber("IR", IR);
+    SmartDashboard.putNumber("dis", dis);
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
